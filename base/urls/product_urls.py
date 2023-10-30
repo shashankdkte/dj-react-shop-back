fe962e2411407ce59d178a1d220619bcdc9c509b
Shashank Dhakate <shashankdkte@gmail.com>
2023-10-31 01:35:30 +0530

    Update Product Endpoint

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("",views.get_products,name="products"),
     path("create/",views.createProduct,name="product_create"),
     path("<str:pk>/",views.get_single_product,name="product_value"),
+
+    path('update/<str:pk>/', views.updateProduct, name="product-update"),
     path("delete/<str:pk>/",views.deleteProduct,name="product_delete"),
 ]
