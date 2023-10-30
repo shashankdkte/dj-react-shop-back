7631f41403499ddac1fd00abdb7f3bc2a41d11d5
Shashank Dhakate <shashankdkte@gmail.com>
2023-10-30 20:14:00 +0530

    Create Product

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -5,6 +5,7 @@ from base.views import product_views as views
 urlpatterns = [
     # path("routes", views.getRoutes, name="routes"),
     path("",views.get_products,name="products"),
+    path("create/",views.createProduct,name="product_create"),
     path("<str:pk>/",views.get_single_product,name="product_value"),
     path("delete/<str:pk>/",views.deleteProduct,name="product_delete"),
 ]
